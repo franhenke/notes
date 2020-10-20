b93c48f68c01c704996a59a1fd699ce50b0deccc
franhenke <franci.henke@gmail.com>
2020-10-20 23:30:03 +0200

    Add onChange and onSubmit to AddContactForm

--- src/components/AddContactForm.js
+++ src/components/AddContactForm.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { GlobalContext } from '../assets/context/GlobalState'
+import { useHistory } from 'react-router-dom'
 import { addContactFormFields } from '../assets/mocks/fields'
 import Input from './Input'
 
 const AddContactForm = () => {
+  const [formData, setFormData] = useState({})
+  const { contacts, addContact } = useContext(GlobalContext)
+  let history = useHistory()
+
+  const handleChange = (event) => {
+    const formDataCopy = { ...formData }
+    formDataCopy[event.target.name] = event.target.value
+    setFormData(formDataCopy)
+    console.log(formData)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    addContact(formData)
+    history.push('/')
+    console.log(contacts)
+  }
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={onSubmit}>
       {addContactFormFields.map((field) => (
         <Input
           key={field.name}
@@ -12,12 +32,12 @@ const AddContactForm = () => {
           name={field.name}
           id={field.id}
           text={field.text}
-          // handleChange={handleChange}
+          handleChange={handleChange}
           placeholder={field.placeholder}
           required={field.required}
         />
       ))}
-      <button>Add contact</button>
+      <button className="button-add-contact">Add contact</button>
     </form>
   )
 }
