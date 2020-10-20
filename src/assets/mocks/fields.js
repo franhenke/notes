ea9e0203efcf5415439885ddfd8f82d4e61178e0
franhenke <franci.henke@gmail.com>
2020-10-20 22:38:53 +0200

    Add fields.js to dynamically generate input fields

---
+++ src/assets/mocks/fields.js
@@ -0,0 +1,81 @@
+const fields = {
+  firstName: {
+    type: 'text',
+    name: 'firstName',
+    text: 'First Name',
+    placeholder: 'Jack',
+    required: false,
+  },
+  lastName: {
+    type: 'text',
+    name: 'lastName',
+    text: 'Last Name',
+    placeholder: 'Doe',
+    required: false,
+  },
+  email: {
+    type: 'email',
+    name: 'email',
+    text: 'Email',
+    placeholder: 'jack@mail.com',
+    required: false,
+  },
+  phone: {
+    type: 'phone',
+    name: 'phone',
+    text: 'Phone',
+    placeholder: '+1 (400)-123456',
+    required: false,
+  },
+  addressLine: {
+    type: 'text',
+    name: 'addressLine',
+    text: 'Address Line',
+    placeholder: 'Awesome str 9',
+    required: false,
+  },
+  postalCode: {
+    type: 'text',
+    name: 'postalCode',
+    text: 'Postal Code',
+    placeholder: '12345',
+    required: false,
+  },
+  city: {
+    type: 'text',
+    name: 'city',
+    text: 'City',
+    placeholder: 'Gothenburg',
+    required: false,
+  },
+  state: {
+    type: 'text',
+    name: 'state',
+    text: 'State',
+    placeholder: 'Gotaland',
+    required: false,
+  },
+  dob: {
+    type: 'date',
+    name: 'dob',
+    text: 'Date of Birth',
+    required: false,
+  },
+  image: {
+    type: 'file',
+    name: 'image',
+    text: 'Upload an Image',
+    required: false,
+  },
+};
+
+export const addContactFormFields = [
+  fields.firstName,
+  fields.lastName,
+  fields.email,
+  fields.addressLine,
+  fields.postalCode,
+  fields.state,
+  fields.dob,
+  fields.image,
+];
