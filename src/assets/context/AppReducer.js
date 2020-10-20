fa0b283172dd77235e1637d4134c31094d740245
franhenke <franci.henke@gmail.com>
2020-10-20 22:39:12 +0200

    Add AppReducer and GlobalState for context

---
+++ src/assets/context/AppReducer.js
@@ -0,0 +1,40 @@
+export default (state, action) => {
+  switch (action.type) {
+    case 'REMOVE_CONTACT':
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          (contact) => contact.id !== action.payload
+        ),
+      };
+    case 'ADD_CONTACT':
+      const nextId = Math.max.apply(
+        null,
+        state.contacts.map((contact) => contact.id)
+      );
+      const newContact = {
+        ...action.payload,
+        id: nextId + 1,
+      };
+      return {
+        ...state,
+        contacts: [...state.contacts, newContact],
+      };
+
+    case 'EDIT_CONTACT':
+      const updatedContact = action.payload;
+      const updatedContacts = state.contacts.map((contact) => {
+        if (contact.id === updatedContact.id) {
+          return updatedContact;
+        }
+        return contact;
+      });
+
+      return {
+        ...state,
+        contacts: updatedContacts,
+      };
+    default:
+      return state;
+  }
+};
---
+++ src/assets/context/GlobalState.js
@@ -0,0 +1,82 @@
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+
+const initialState = {
+  contacts: [
+    {
+      id: 1,
+      firstName: 'Kate',
+      lastName: 'Edison',
+      email: 'Sincere@april.biz',
+      phone: '1-770-736-8031 x56442',
+      addressLine: 'Boston Str. 123',
+      postalCode: 'BO123AZ',
+      city: 'Boston',
+      state: 'Massachusetts',
+      birthday: '10.10.1988',
+      image:
+        'https://res.cloudinary.com/frnsea/image/upload/v1603225955/Sarah_suhilg.jpg',
+    },
+    {
+      id: 2,
+      firstName: 'Oliver',
+      lastName: 'Grayson',
+      email: 'Sincere@april.biz',
+      phone: '1-770-736-8031 x56442',
+      addressLine: '13th Street, 47 W 13th St',
+      postalCode: 'NY 10011',
+      city: 'New York City',
+      state: 'New York',
+      birthday: '03.09.1979',
+      image:
+        'https://res.cloudinary.com/frnsea/image/upload/v1603226163/oliver_n6mo28.jpg',
+    },
+  ],
+};
+
+export const GlobalContext = createContext(initialState);
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  function removeContact(id) {
+    dispatch({
+      type: 'REMOVE_CONTACT',
+      payload: id,
+    });
+  }
+
+  function addContact(contacts) {
+    dispatch({
+      type: 'ADD_CONTACT',
+      payload: contacts,
+    });
+  }
+
+  function addPhoto(contacts) {
+    dispatch({
+      type: 'ADD_PHOTO',
+      payload: contacts,
+    });
+  }
+
+  function editContact(contacts) {
+    dispatch({
+      type: 'EDIT_CONTACT',
+      payload: contacts,
+    });
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        contacts: state.contacts,
+        removeContact,
+        addContact,
+        addPhoto,
+        editContact,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
