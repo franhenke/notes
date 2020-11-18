7df9484cc6a3a33fc7a4f40189c0b93bef9c7471
franhenke <franci.henke@gmail.com>
2020-11-18 13:47:21 +0100

    Creating LoadingScreen

---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LoadingScreen = () => {
+  return (
+    <div className="load">
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+    </div>
+  )
+}
+
+export default LoadingScreen
