29614d97902d4d368f3c10fd013cec1f66c18ada
franhenke <franci.henke@gmail.com>
2020-11-24 16:54:30 +0100

    Add Navigation styling for desktops

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,9 @@ import React from 'react'
 import Navigation from './Navigation'
 
 const Header = () => {
-  const userName = 'Sarah'
-
   return (
     <header className="header">
       <Navigation />
-      <h3>Hello {userName}</h3>
     </header>
   )
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import { Link, useParams } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import * as ROUTES from './../assets/routes'
 
 const Navigation = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
-    <nav>
-      <Link to={ROUTES.HOME}>Home</Link>
-      <Link to={ROUTES.CONTACTS}>Contacts</Link>
-      <Link to={ROUTES.NOTES}>Notes</Link>
-      <Link to={ROUTES.DATES}>Dates</Link>
+    <nav className="navigation">
+      <span>mates</span>
+      <div className="menu-content-container">
+        <div className="links">
+          <Link to={ROUTES.HOME}>Home</Link>
+          <Link to={ROUTES.CONTACTS}>Contacts</Link>
+          <Link to={ROUTES.NOTES}>Notes</Link>
+          <Link to={ROUTES.DATES}>Dates</Link>
+        </div>
+      </div>
     </nav>
   )
 }
