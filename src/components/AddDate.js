58054d63acbd4870c058977f233d9cffe4cdd343
franhenke <franci.henke@gmail.com>
2020-11-29 23:46:10 +0100

    Change styling of AddDate form

--- src/components/AddDate.js
+++ src/components/AddDate.js
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import 'antd/dist/antd.css'
 import { DatePicker } from 'antd'
-import moment from 'moment'
+import cancelIcon from './../assets/icons/cross.svg'
 import * as ROUTES from './../assets/routes'
 import { GlobalContext } from '../assets/context/GlobalState'
 import useForm from '../hooks/useForm'
@@ -24,22 +24,49 @@ const AddDate = () => {
 
   return (
     <div className="add-date_form-container">
+      <div className="contact-button-cancel">
+        <Link to={ROUTES.HOME}>
+          <img src={cancelIcon} alt="cancel" />
+        </Link>
+      </div>
       <form onSubmit={handleSubmit} className="add-date-form">
         <h4>Add new date</h4>
-        <DatePicker
-          showTime={{ minuteStep: 15 }}
-          format="dddd, MMM D YYYY, HH:mm"
-          onChange={onChange}
-        />
-        <label htmlFor="where">Occasion</label>
+
+        <label htmlFor="occasion">Occasion</label>
         <input
           type="text"
           name="occasion"
-          placeholder="e.g. Lunch, Dinner etc"
+          placeholder="e.g. Lunch, Dinner"
           value={values.occasion || ''}
           onChange={handleChange}
           required
         />
+        <label htmlFor="details">Details</label>
+        <input
+          type="text"
+          name="details"
+          placeholder="more..."
+          value={values.details || ''}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="date" className="add-date_details">
+          Date & Time
+        </label>
+        <DatePicker
+          className="datepicker"
+          name="date"
+          showTime={{ minuteStep: 15 }}
+          format="dddd, MMM D YYYY, HH:mm"
+          onChange={onChange}
+          style={{
+            padding: '0px',
+            margin: '0 0 2em 0',
+            cursor: 'pointer',
+          }}
+          size={15}
+        />
+
         <label htmlFor="where">Where</label>
         <input
           type="text"
@@ -47,13 +74,11 @@ const AddDate = () => {
           value={values.where || ''}
           onChange={handleChange}
         />
+
         <button type="submit" className="button-add">
           Add
         </button>
       </form>
-      <div className="contact-button-cancel">
-        <Link to={ROUTES.HOME}>Cancel</Link>
-      </div>
     </div>
   )
 }
