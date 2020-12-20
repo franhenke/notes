77a58cf3e1df9d244af17bb41290f6a3c67f884f
franhenke <franci.henke@gmail.com>
2020-12-20 17:49:52 +0100

    Create contactController and clean up contactRoutes

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,63 +1,15 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import Contact from '../models/contactModel.js'
+import {
+  getContacts,
+  getFavoriteContacts,
+  getContactById,
+  deleteContact,
+} from '../controllers/contactController.js'
 
-// @desc    Fetch all contacts
-// @route   GET /contacts
-// @access  Public
-router.get(
-  '/contacts',
-  asyncHandler(async (req, res) => {
-    const contacts = await Contact.find({})
-    res.json(contacts)
-  })
-)
-
-// @desc    Fetch single contacts
-// @route   GET /contacts/:id
-// @access  Public
-router.get(
-  '/contacts/:id',
-  asyncHandler(async (req, res) => {
-    const contact = await Contact.findById(req.params.id)
-
-    if (contact) {
-      res.json(contact)
-    } else {
-      res.status(404)
-      throw new Error('Product not found')
-    }
-  })
-)
-// @desc    Fetch favorite contacts
-// @route   GET /contacts
-// @access  Public
-router.get(
-  '/home',
-  asyncHandler(async (req, res) => {
-    const contacts = await Contact.find({ favorite: true })
-    res.json(contacts)
-  })
-)
-
-router.delete(
-  '/contacts/:id',
-  asyncHandler(async (req, res) => {
-    try {
-      let contact = await Contact.findById(req.params.id)
-
-      if (!contact)
-        return res.status(404).json({ message: 'Contact not found.' })
-
-      await Contact.findByIdAndRemove(req.params.id)
-
-      res.json({ message: 'Contact has been removed.' })
-    } catch (error) {
-      console.error(error.message)
-      res.status(500).send('Server Error')
-    }
-  })
-)
+router.route('/home').get(getFavoriteContacts)
+router.route('/contacts').get(getContacts)
+router.route('/contacts/:id').get(getContactById)
+router.route('/contacts/:id').delete(deleteContact)
 
 export default router
