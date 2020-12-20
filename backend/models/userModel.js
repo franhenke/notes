67bafd8b2680960fe11e50a99ef34a62f491454c
franhenke <franci.henke@gmail.com>
2020-12-20 19:03:37 +0100

    Create generateToken.js and add to userModel and userController

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,12 +1,9 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema(
   {
     firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
       type: String,
       required: true,
     },
@@ -24,6 +21,10 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 )
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
 
 const User = mongoose.model('User', userSchema)
 export default User
