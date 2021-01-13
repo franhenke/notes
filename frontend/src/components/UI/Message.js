d44632619567f79ad7d10c1b2ce149fe50791c94
franhenke <franci.henke@gmail.com>
2021-01-13 14:34:13 +0100

    Create FormContainer for the login form and message component for error handling

--- frontend/src/components/UI/Message.js
+++ frontend/src/components/UI/Message.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Alert } from 'react-bootstrap'
+import Alert from 'react-bootstrap/Alert'
 
 const Message = ({ variant, children }) => {
   return <Alert variant={variant}>{children}</Alert>
